0adcbc33111ff318890504ad00f155861ce72fc3
Morgan Nelson <mnelson@steele-forge.com>
2012-08-08 15:58:42 -0500

    remove defaults as some blocks are not required by all

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,20 +1,11 @@
-default['appshot']['name']                                = "default mysql"
+default['appshot']['name']                                = "default"
 default['appshot']['crontab']['hour']                     = 1
 default['appshot']['crontab']['minute']                   = 1
 default['apphsot']['crontab']['command']                  = "appshot"
 
-default['appshot']['database']['type']                    = "mysql"
-default['appshot']['database']['name']                    = "mysql"
-default['appshot']['database']['password']                = "password"
-default['appshot']['database']['port']                    = 3306
-default['appshot']['database']['username']                = "username"
-
-default['appshot']['filesystem']['type']                  = "xfs"
-default['appshot']['filesystem']['mount_point']           = "/data/mysql"
-
-default['appshot']['aws']['access_key_id']                = "your_aws_access_key"
-default['appshot']['aws']['secret_access_key']            = "your_aws_secret_access"
-default['appshot']['ebs_prune']['snapshots_to_keep']      = 5
-default['appshot']['ebs_prune']['minimum_retention_days'] = 3
-default['appshot']['ebs_snapshot']['volume_id']           = "vol-some_id"
+default['appshot']['database']
+default['appshot']['filesystem']
+default['appshot']['aws']
+default['appshot']['ebs_snapshot']
+default['appshot']['ebs_prune']
 
